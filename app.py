2f5000697d16ab02fe2cb7fd37748a9a57fc41d1
Afnan <anan77.afnan@gmail.com>
2026-01-11 22:17:16 +0800

    "calculator for thermal expansion practice"

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-number = (2,2,4,6,3,4,6,1)
-uniques = []
-for number in number:
-    if number not in uniques:
-        uniques.append(number)
-print(uniques)
+
+early_temp = int(input("Early Temperature:  "))
+last_temp = int(input("Last Temperature:  "))
+change_in_temp = int(last_temp - early_temp)
+alpha_aluminium = float(0.0000128)
+L = int(input("L: "))
+thermal_expansion = (alpha_aluminium * L * change_in_temp )
+print(f"Thermal expansion is: {thermal_expansion} mm ")
